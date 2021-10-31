1bad19072779e01722dafdfc3a0f1e6c5c45ac7e
felipedias55 <52840562+felipedias55@users.noreply.github.com>
2021-10-31 17:14:25 -0300

    Criação de backend, server e contato

---
+++ backend/contato/contato.js
@@ -0,0 +1,35 @@
+let enviar = document.getElementById("enviar");
+
+let nome = document.getElementById('formGroupExampleInput');
+let email = document.getElementById('exampleFormControlInput1');
+let telefone = document.getElementById('formGroupExampleInput2');
+let mensagem = document.getElementById('exampleFormControlTextarea1');
+
+
+enviar.onclick = function(){
+    
+    let dadosFormulario = {
+        nome: nome.value,
+        email: email.value,
+        telefone: telefone.value,
+        mensagem: mensagem.value
+    }
+
+    let xhr = new XMLHttpRequest();
+    xhr.open('POST', '/');
+    xhr.setRequestHeader('content-type','application/json');
+    xhr.onload = function(){
+        console.log(xhr.responseText);
+        if(xhr.responseText = "success"){ 
+            alert('Email enviado!');
+            nome.value = '';
+            email.value = '';
+            telefone.value = '';
+            mensagem.value = '';
+        }else{
+            alert('Algo deu errado!')
+        }
+    }
+
+    xhr.send(JSON.stringify(dadosFormulario));
+}
---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const app = express();
+
+const nodemailer = require('nodemailer');
+
+const PORT = process.env.PORT || 5000;
+
+app.use(express.static('./')); 
+app.use(express.json());
+
+app.get('/', (req, res) =>{
+    res.sendFile(__dirname + '/index.html');
+})
+
+app.post('/', (req,res) =>{
+    console.log(req.body);
+
+    const transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port:587,
+        secure:false,
+        auth:{
+            user: "covidonation@gmail.com",
+            pass: "covidonation123",
+        },
+        tls:{
+            rejectUnauthorized: false,
+        }
+    
+    });
+
+
+    transporter.sendMail({
+        from: req.body.nome,
+        to: "covidonation@gmail.com",
+        subject: "Mensagem de contato",
+        text: req.body.mensagem +"\n Mensagem enviada por:"+"\n Telefone: "+req.body.telefone+
+        "\n Nome:"+req.body.nome,
+        replyTo: req.body.email
+    }).then(message =>{
+        console.log(message);
+    }).catch(err => {
+        console.log(err);
+    })
+
+})
+
+app.listen(process.env.PORT || 5000);
